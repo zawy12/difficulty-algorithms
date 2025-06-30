815c390bcd3596aba1cbd0cd607e309c8a0121d9
zawy12 <zawy@yahoo.com>
2025-06-30 08:51:41 -0400

    Update chain_work.cpp

--- chain_work.cpp
+++ chain_work.cpp
@@ -21,16 +21,6 @@ is the result of the Erlang distribution of N exponential solvetimes.
 
 Sum(Difficulty) * (N-1)/N is perfect only if difficulty is constant.  
 
-The only adjustment to HR I have been able to think of that includes the "time since last block" works like this. 
-> If a new block shows up now and it decreases the HR from the current estimate, then assume it just showed up and apply that adjustment."  
-This means doing the following.
-
-tslb = time since last block
-if ....
-N/(sum ST/D)*(N-1)/N > (N+1)/(sum ST/D + tslb/current_D)*N/(N+1)
-then ....
-HR = (N+1)/(sum ST/D + tslb/current_D)*N/(N+1)
-
 */
 #include <iostream> 
 #include <vector>
@@ -49,7 +39,7 @@ d print_out (d work, d HR, d H, string name) {
 		return 0;
 }
 
-d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
+d run_simulation( long int TIPS, vector<d>D, vector<d>HR) {
 	// Back of vector D is the difficulty that's not yet solved.
 	d current_D = D.back(); 	D.pop_back();
 	d current_HR = HR.back();  HR.pop_back();
@@ -58,6 +48,8 @@ d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
 	d N = D.size(), sum_Ds=0, avg_HR=0, ST=0, avg_ST=0;
 	d sum_ST=0, sum_actual_work=0, sum_TT_inv_HR=0; 
 	d avg_Z_HR=0, avg_Z_work=0, actual_work=0, avg_actual_HR=0, avg_actual_work=0;
+	d avg_ZZ_work=0, avg_ZZ_HR=0;
+	d avg_actual_ZZ_work=0, avg_actual_ZZ_HR=0;
 	d avg_chain_work_HR=0, harmonic_sum=0, avg_H_work=0, avg_H_HR=0; // Harmonic difficulties 
 	d sum_TT_easiness=0, avg_TT_work=0, avg_TT_HR=0; // target*time 
 	d sum_S_easiness=0, avg_S_easiness=0, avg_S_HR_ease=0; // entropy 
@@ -74,11 +66,11 @@ d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
 		avg_ST += sum_ST / TIPS; // avg TIP ST
 	}
 	for (int j = 0; j < N; j++ ) { 	
-		D[j]	= D[j]*TARGET_TIME/(avg_ST+tslb); 
+		D[j]	= D[j]*TARGET_TIME/avg_ST; 
 		sum_Ds	+= D[j];
 	}
-	current_D = current_D*TARGET_TIME/(avg_ST+tslb);
-	// Adjust D and HR so that sum_D=5 without changing TARGET_TIME.
+	current_D = current_D*TARGET_TIME/avg_ST;
+	// Adjust D and HR so that sum_D = 5 without changing TARGET_TIME.
 	for (int j = 0; j < N; j++)  {
 		D[j]	= D[j]*5/sum_Ds;
 		HR[j]	= HR[j]*5/sum_Ds;		
@@ -92,11 +84,11 @@ d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
 	for ( int j=0; j<N; j++) {  
 		cout << D[j] << ", ";
 		sum_Ds += D[j];
-		avg_HR += D[j]*HR[j];
+	//	avg_HR += D[j]*HR[j]; // temporary calculation to get difficulty-weighted avg_HR. 
 		harmonic_sum += 1/D[j];  
 	}
 	cout << " unsolved D: " << current_D;
-	avg_HR = avg_HR / sum_Ds;
+	// avg_HR = avg_HR / sum_Ds;
 	cout << "\nHR: ";
 	for (int j=0; j<N; j++) { cout << HR[j] << ", "; }
 	cout << " unsolved HR: " << current_HR;
@@ -113,9 +105,9 @@ d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
 			sum_TT_easiness	+= ST/D[j];
 		}
 		// means of actual work and hashrates and TT
-		m_A_w	+= (sum_actual_work + tslb*current_HR)/ TIPS; 
-		m_A_hr	+= (sum_actual_work + tslb*current_HR)/(sum_ST+tslb) / TIPS;
-		m_TT_hr	+= N/sum_TT_easiness*(N-1)/N/ TIPS; // no tslb
+		m_A_w	+= sum_actual_work / TIPS; 
+		m_A_hr	+= sum_actual_work / sum_ST / TIPS;
+		m_TT_hr	+= N/sum_TT_easiness*(N-1)/N / TIPS;
 	}
 
 	for (long int i=1; i <= TIPS ; i++) {
@@ -125,6 +117,8 @@ d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
 		sum_S_easiness = 0;
 		sum_harmonic_work = 0;
 		actual_work = 0;
+		d final_ST = 0;
+		d each_interval = 0;
 		// The following sums are to get avgs PER TIP.
 		// Avg ST != avg 1/ST for small samples due to exponential distribution
 		// more likely to have fast solvetimes.
@@ -133,66 +127,48 @@ d run_simulation( long int TIPS, vector<d>D, vector<d>HR, d tslb) {
 			ST = D[j]/HR[j] * log(1/fRand(0,1)); 
 			// ST = std::max(0.002,double(ST)); // To simulate no < 1 sec solves with T=500
 			sum_ST			+= ST;
-			sum_actual_work	+= ST*HR[j];  // This is an observation of hashes = D[j]*log(1/rand)
+			actual_work		+= ST*HR[j];  // Notice this is just going to be sum of D's
 			sum_TT_easiness += ST/D[j]; // TT = target*time. time*(P of finding a block) = 1/HR[j]*log(1/rand)
 			sum_S_easiness	+= log(ST/D[j]);
 		}
-		avg_ST				+= sum_ST/TIPS; // without tslb
-		actual_work		= sum_actual_work + tslb*current_HR;
+		final_ST 	= D[N-1]/HR[N-1] * log(1/fRand(0,1));		
+		avg_actual_ZZ_work 	+= (actual_work + final_ST*HR[N-1])/TIPS;
+		avg_actual_ZZ_HR 	+= (actual_work + final_ST*HR[N-1])/(sum_ST + final_ST)/TIPS;
+
+		avg_ST				+= sum_ST/TIPS; 
 		avg_actual_work		+= actual_work / TIPS; 
-		avg_actual_HR		+= actual_work /(sum_ST+tslb) / TIPS;
-		avg_chain_work_HR	+= sum_Ds/sum_ST / TIPS;	// no tslb adjustment
+		avg_actual_HR		+= actual_work /sum_ST / TIPS;
+		avg_chain_work_HR	+= sum_Ds/sum_ST / TIPS;	
 
 		sd_A_w	+= (actual_work - m_A_w)*(actual_work - m_A_w) / TIPS;
 		sd_A_hr	+= (actual_work/sum_ST - m_A_hr)*(actual_work/sum_ST - m_A_hr) / TIPS;
 
 		TT_w	= N/sum_TT_easiness*(N-1)/N;
-		sd_TT_hr	+= (TT_w/sum_ST - m_TT_hr)*(TT_w/sum_ST - m_TT_hr) / TIPS; // no tslb
+		sd_TT_hr	+= (TT_w/sum_ST - m_TT_hr)*(TT_w/sum_ST - m_TT_hr) / TIPS; 
 
-		// This is a HR adustment if it's been a long time since last block passed by.
-		// If the time since last block is such that a N+1 block arriving now 
-		// would reduce the HR, then apply that time, difficulty, & N+1 as if a block 
-		// just arrived. Can be ignored if tslb = 0. Suggestion: set tslb = 0.
-		// The if statement assumes TT is the correct way to decide.
-		if (N/sum_TT_easiness*(N-1)/N > (N+1)/(sum_TT_easiness + tslb/current_D)*((N+1)-1)/(N+1) ) {
+		avg_Z_work		+= sum_Ds / TIPS; // for code symmetry
+		avg_Z_HR		+= sum_Ds / sum_ST * (N-1)/N / TIPS ; 
 
-			avg_Z_work		+= (sum_Ds + current_D) / TIPS; // for code symmetry
-			avg_Z_HR		+= (sum_Ds + current_D)/(sum_ST+tslb)*((N+1)-1)/(N+1)/ TIPS;
+		avg_ZZ_work 	+= sum_Ds / TIPS;			
+		avg_ZZ_HR 		+= sum_Ds / (sum_ST+final_ST) / TIPS ;
 			
-			avg_H_work	+= (N+1)*(N+1)/(harmonic_sum + 1/current_D)/ TIPS;
-			avg_H_HR	+= (N+1)*(N+1)/(harmonic_sum + 1/current_D)/(sum_ST+tslb)*((N+1)-1)/(N+1)/ TIPS;
+		avg_H_work		+= N*N/harmonic_sum / TIPS; //  is smaller than sum Ds if Ds vary.
+		avg_H_HR		+= N*N/harmonic_sum/sum_ST*(N-1)/N / TIPS;
 
-			avg_TT_work		+= (N+1)/(sum_TT_easiness + tslb/current_D)  * sum_ST/ TIPS;
-			avg_TT_HR		+= (N+1)/(sum_TT_easiness + tslb/current_D)*((N+1)-1)/(N+1)/ TIPS;
-	
-			avg_S_HR_ease	+= (sum_S_easiness + log(tslb/current_D))/log(2)*N/(N+1)  / TIPS;
-		}
-		else { 
-			avg_Z_work		+= sum_Ds / TIPS; // for code symmetry
-			avg_Z_HR		+= sum_Ds / sum_ST * (N-1)/N / TIPS ;  
-			
-			avg_H_work		+= N*N/harmonic_sum / TIPS; //  is smaller than sum Ds if Ds vary.
-			avg_H_HR		+= N*N/harmonic_sum/sum_ST*(N-1)/N / TIPS;
+		avg_TT_work		+= N/sum_TT_easiness * sum_ST / TIPS ;
+		avg_TT_HR		+= N/sum_TT_easiness*(N-1)/N / TIPS;	
 
-			avg_TT_work	+= N/sum_TT_easiness * sum_ST / TIPS ;
-			avg_TT_HR		+= N/sum_TT_easiness*(N-1)/N / TIPS;	
-
-			avg_S_HR_ease	+=  sum_S_easiness / log(2) *(N-1)/N / TIPS;
-		 }
 	}
 	cout << int(N) << " blocks took " << avg_ST << " solvetimes.\n";
-	cout << "Time since last block: " << tslb << " solvetimes.\n";
-	cout << "A total of " << avg_ST+tslb << " solvetimes since split.\n";
-	if (tslb*current_HR/current_D > avg_ST*avg_actual_HR/avg_actual_work) { 
-		cout << "\nInterpretation warning: Last block delay is > 50% unlikely.\nActual values are not averages for the given HR.\n"; 
-	}
+	cout << "A total of " << avg_ST << " solvetimes since split.\n";
 	cout << "\n" << avg_actual_work << " (work), " <<  
-		avg_actual_HR << " (HR) Input data" << endl;	
-	print_out(sum_Ds, avg_chain_work_HR,   avg_actual_HR,  "HR = (sum Ds)/(sum STs)");
-	print_out(avg_Z_work, avg_Z_HR,  avg_actual_HR,  "HR = (sum Ds)/(sum STs)*(N-1)/N incl. delay");
+		avg_actual_HR << " (HR) Input data" << "\nZZ actual work: " << avg_actual_ZZ_work << endl;	
+	print_out(sum_Ds, avg_chain_work_HR,  avg_actual_HR,  "HR = (sum Ds)/(sum STs)");
+	print_out(avg_Z_work, avg_Z_HR,  avg_actual_HR,  "HR = (sum Ds)/(sum STs)*(N-1)/N");
+	print_out(avg_ZZ_work, avg_ZZ_HR, avg_actual_ZZ_HR, "ZZ HR = (sum Ds)/(final_ST + sum STs)");
 	// print_out(avg_H_work, avg_H_HR,  avg_actual_HR,  "harmonic_mean_Ds/avg_STs *(N-1)/N incl. delay");
-	print_out(avg_TT_work, avg_TT_HR,  avg_actual_HR,  "harmean(D/ST)*(N-1)/N incl. delay (Best HR, work is iffy)");
-	// print_out(0, avg_S_HR_ease,  avg_S_HR_ease,  "entropy of harmean(D/ST)*(N-1)/N. Lower entropy = earlier tip.");
+	print_out(avg_TT_work, avg_TT_HR,  avg_actual_HR,  "harmean(D/ST)*(N-1)/N  (Best HR, work is iffy)");
+	
 	cout << "\n"; 
 	return 0;
 }
@@ -204,9 +180,6 @@ cout << fixed << setprecision(2);
 cout << "Hashreate (HR) = 1 hash/(target solvetime)\nTarget solvetime = 1\n";
 cout << "Difficulty = 2^256/target = avg # hashes to solution = 1/(Probability per hash)\n";
 
-// The following should be 0 except in advanced testing.
-d tslb = 0; // Time Since Last Block as fraction of target solvetime
-
 // TARGET_TIME is current time minus time of split. It is used to 
 // adjust the difficulties so that all the tips have the same current time. 
 // It will also adjust HRs so that chain work = 1. This allows
@@ -216,29 +189,27 @@ d tslb = 0; // Time Since Last Block as fraction of target solvetime
 
 TARGET_TIME = 0;
 
-// Last element of vector D and HR are an unsolved block
-// The last element is used if tslb != 0
-// These difficulties & hashrates are scaled in the outputs for tslb reasons.
+// These difficulties & hashrates are scaled in the outputs for an historical reason.
 vector<d>D  = {1,1,1, 1}; // Difficulties
 vector<d>HR = {1,1,1, 1}; // Hashrate
 
 for (int i=0; i<HR.size(); i++) { TARGET_TIME += D[i]/HR[i];  }
 
-run_simulation(TIPS, D, HR, tslb);
+run_simulation(TIPS, D, HR);
 D  = {1,1,2,2, 1}; 
 HR = {1,1,1,1, 1};
-run_simulation(TIPS, D, HR, tslb);
+run_simulation(TIPS, D, HR);
 D  = {1,1,1,1, 1}; 
 HR = {1,3,3,1, 1};
-run_simulation(TIPS, D, HR, tslb);
+run_simulation(TIPS, D, HR);
 D  = {1,1,1,1, 1}; 
 HR = {2,2,2,2, 2};
-run_simulation(TIPS, D, HR, tslb);
+run_simulation(TIPS, D, HR);
 D  = {3,3,1,1,1}; 
 HR = {2,2,1,1, 1};
-run_simulation(TIPS, D, HR, tslb);
+run_simulation(TIPS, D, HR);
 D  = {3,3,1,1, 1}; 
 HR = {1,1,2,2, 1};
-run_simulation(TIPS, D, HR, tslb);
+run_simulation(TIPS, D, HR);
 exit(0);
 }
